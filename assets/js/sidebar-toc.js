463faaced5298ff320438d7f814083cbae22216e
eyssette <cedric.eyssette@gmail.com>
2022-10-13 11:21:43 +0200

    Update sidebar-toc.js

--- assets/js/sidebar-toc.js
+++ assets/js/sidebar-toc.js
@@ -21,40 +21,4 @@ window.addEventListener("load", function () {
 		pagetocLink.setAttribute("data-referrer", header.id);
 		pagetoc.appendChild(pagetocLink);
 	});
-
-	// Active pagetoc element on scroll
-	window.addEventListener(
-		"scroll",
-		function () {
-			var pagetoc = document.getElementsByClassName("pagetoc")[0];
-			var activeLink = pagetoc.getAttribute("data-active-link");
-
-			// find use position and get activeEl
-			var headers = document.querySelectorAll("h1, h2, h3, h4, h5, h6");
-			var lastHeader;
-			for (var header of headers) {
-				if (window.pageYOffset >= header.offsetTop) {
-					lastHeader = header;
-				} else {
-					break;
-				}
-			}
-			// if same, no need do anything
-			if (activeLink == lastHeader.id) {
-				return;
-			}
-			activeLink = lastHeader.id;
-			pagetoc.setAttribute("data-active-link", activeLink);
-
-			// Set pagetoc active element
-			Array.prototype.forEach.call(pagetoc.children, function (pagetocLink) {
-				if (activeLink == pagetocLink.getAttribute("data-referrer")) {
-					pagetocLink.classList.add("active");
-				} else {
-					pagetocLink.classList.remove("active");
-				}
-			});
-		},
-		false
-	);
 });
