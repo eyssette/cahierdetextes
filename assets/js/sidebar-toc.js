6e204b19e35e56b6d0035d6d8526237dffae9a4a
eyssette <cedric.eyssette@gmail.com>
2022-10-13 12:54:08 +0200

    Update sidebar-toc.js

--- assets/js/sidebar-toc.js
+++ assets/js/sidebar-toc.js
@@ -1,6 +1,12 @@
 // Source : https://github.com/evan361425/evan361425.github.io/blob/acba613d2071c34075c6f1a3bff4d781dea49b71/pagetoc.js
 // Referrer: https://github.com/JorelAli/mdBook-pagetoc/blob/master/sidebar.js
 // Populate sidebar on load
+function pairwise(arr, func){
+    for(var i=0; i < arr.length - 1; i++){
+        func(arr[i], arr[i + 1])
+    }
+}
+
 window.addEventListener("load", function () {
 	var pagetoc = document.getElementsByClassName("pagetoc")[0];
 	var headers = document.querySelectorAll("h1, h2, h3, h4, h5, h6");
@@ -33,27 +39,32 @@ window.addEventListener("load", function () {
 			// Get current scroll position
 			let scrollY = window.pageYOffset;
 
+
+			pairwise(Array.from(headers), function(current, next){
+				console.log(current, next)
+			})
+			
+
 			// Now we loop through headers to get height, top and ID values for each
-			headers.forEach(current => {
-				const sectionHeight = current.offsetHeight;
-				const sectionTop = current.offsetTop - 50;
-				sectionId = current.getAttribute("id");
+			// headers.forEach(current => {
+			//	const sectionTop = current.offsetTop-10;
+			//	sectionId = current.getAttribute("id");
 
 				/*
 				- If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation link, else remove it
 				- To know which link needs an active class, we use sectionId variable we are getting while looping through headers as an selector
 				*/
-				if (
+			/* 	if (
 					scrollY > sectionTop &&
 					scrollY <= sectionTop + sectionHeight
 				) {
-					console.log('yes' + sectionId);
 					document.querySelector("nav a[href*=" + sectionId + "]").classList.add("active");
 				} else {
-					console.log('no' + sectionId);
 					document.querySelector("nav a[href*=" + sectionId + "]").classList.remove("active");
 				}
-			});
+			}); */
+
+			
 		});
 
 });
