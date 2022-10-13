a86d853788e92e04b2062a76767f99322efb474e
eyssette <cedric.eyssette@gmail.com>
2022-10-13 12:42:50 +0200

    Update sidebar-toc.js

--- assets/js/sidebar-toc.js
+++ assets/js/sidebar-toc.js
@@ -23,8 +23,6 @@ window.addEventListener("load", function () {
 	});
 
 
-	// Get all sections that have an ID defined
-	const sections = document.querySelectorAll("section[id]");
 	// https://codepen.io/malsu/pen/VwKzoPG
 	// Add an event listener listening for scroll
 	window.addEventListener(
@@ -35,15 +33,15 @@ window.addEventListener("load", function () {
 			// Get current scroll position
 			let scrollY = window.pageYOffset;
 
-			// Now we loop through sections to get height, top and ID values for each
-			sections.forEach(current => {
+			// Now we loop through headers to get height, top and ID values for each
+			headers.forEach(current => {
 				const sectionHeight = current.offsetHeight;
 				const sectionTop = current.offsetTop - 50;
 				sectionId = current.getAttribute("id");
 
 				/*
 				- If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation link, else remove it
-				- To know which link needs an active class, we use sectionId variable we are getting while looping through sections as an selector
+				- To know which link needs an active class, we use sectionId variable we are getting while looping through headers as an selector
 				*/
 				if (
 					scrollY > sectionTop &&
