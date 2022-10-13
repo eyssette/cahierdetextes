73d7861ac5b144c70f6af5676059e0e4d36db4e6
eyssette <cedric.eyssette@gmail.com>
2022-10-13 09:43:05 +0200

    Update sidebar-toc.js

--- assets/js/sidebar-toc.js
+++ assets/js/sidebar-toc.js
@@ -1,3 +1,6 @@
+// Source : https://github.com/evan361425/evan361425.github.io
+// Referrer: https://github.com/JorelAli/mdBook-pagetoc/blob/master/sidebar.js
+// Populate sidebar on load
 window.addEventListener("load", function () {
 	var pagetoc = document.getElementsByClassName("pagetoc")[0];
 	var headers = document.querySelectorAll("h1, h2, h3, h4, h5, h6");
@@ -8,15 +11,15 @@ window.addEventListener("load", function () {
 
 	Array.prototype.forEach.call(headers, function (header) {
 		var pagetocLink = document.createElement("a");
-		var tagName = header.parentElement.tagName.toLowerCase();
+		var tagName = header.tagName.toLowerCase();
 		// no need h1, h5~h6
 		if (!["h2"].includes(tagName)) return;
 
 		pagetocLink.appendChild(document.createTextNode(header.text));
 		pagetocLink.classList.add(tagName);
-		pagetocLink.href = header.href;
-		pagetocLink.setAttribute("data-referrer", header.parentElement.id);
-		console.log(pagetocLink);
+		pagetocLink.href = '#'+header.tagName;
+		pagetocLink.setAttribute("data-referrer", header.id);
+
 		pagetoc.appendChild(pagetocLink);
 	});
 
@@ -28,7 +31,7 @@ window.addEventListener("load", function () {
 			var activeLink = pagetoc.getAttribute("data-active-link");
 
 			// find use position and get activeEl
-			var headers = document.getElementsByClassName("header");
+			var headers = document.querySelectorAll("h1, h2, h3, h4, h5, h6");
 			var lastHeader;
 			for (var header of headers) {
 				if (window.pageYOffset >= header.offsetTop) {
@@ -38,10 +41,10 @@ window.addEventListener("load", function () {
 				}
 			}
 			// if same, no need do anything
-			if (activeLink == lastHeader.parentElement.id) {
+			if (activeLink == lastHeader.id) {
 				return;
 			}
-			activeLink = lastHeader.parentElement.id;
+			activeLink = lastHeader.id;
 			pagetoc.setAttribute("data-active-link", activeLink);
 
 			// Set pagetoc active element
